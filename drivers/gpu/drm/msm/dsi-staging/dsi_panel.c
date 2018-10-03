dc004c7e644a63342b85469d8a371b960fc2a5c7
Bang Nguyen <bangnguyen@motorola.com>
2018-10-03 17:13:51 +0800

    drm/msm/dsi-staGing: Support 2-bytes DCS backlight
    Support 2bytes DCS backlight for AMOLED display.
    Enable the function in the dts file.
    
    Change-Id: I0f5ded29ac589aa53e87b57bed7f8031908e092d
    Signed-off-by: daiss1 <daiss1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1339849
    SLTApproved: Slta Waiver
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -624,10 +624,15 @@ static int dsi_panel_update_backlight(struct dsi_panel *panel,
 	if (panel->bl_config.bl_inverted_dbv)
 		bl_lvl = (((bl_lvl & 0xff) << 8) | (bl_lvl >> 8));
 
-	if (bl->bl_2bytes_0xff0f_enable)
+
+       if (bl->bl_2bytes_0xff0f_enable)
 		bl_lvl = ((bl_lvl & 0x000f) << 8) |(bl_lvl >> 4);
 
-	rc = mipi_dsi_dcs_set_display_brightness(dsi, bl_lvl);
+	if (bl->bl_2bytes_enable)
+		rc = mipi_dsi_dcs_set_display_brightness_2bytes(dsi, bl_lvl);
+	else
+		rc = mipi_dsi_dcs_set_display_brightness(dsi, bl_lvl);
+
 	if (rc < 0)
 		pr_err("failed to update dcs backlight:%d\n", bl_lvl);
 
@@ -2336,6 +2341,13 @@ static int dsi_panel_parse_bl_config(struct dsi_panel *panel)
 
 	pr_info("[%s] bl_2bytes_0xff0f_enable=%d\n", panel->name,
 					panel->bl_config.bl_2bytes_0xff0f_enable);
+
+	panel->bl_config.bl_2bytes_enable = utils->read_bool(utils->data,
+			"qcom,bklt-dcs-2bytes-enabled");
+
+	pr_info("[%s] bl_2bytes_enable=%d\n", panel->name,
+					panel->bl_config.bl_2bytes_enable);
+
 
 	if (panel->bl_config.type == DSI_BACKLIGHT_PWM) {
 		rc = dsi_panel_parse_bl_pwm_config(panel);
