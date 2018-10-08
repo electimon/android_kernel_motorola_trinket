c18854776a814d718c1d58a67247f6e1c523ab3e
Bang Nguyen <bangnguyen@motorola.com>
2018-10-08 19:48:53 +0800

    drm/msm/dsi-staGing: Reducing logGing in dsi_panel.c
    Reducing unnecessary logGing in dsi_panel.c file.
    Add debug log.
    
    Change-Id: Ibbe08b1ad15e92d6c8846c9512b52f2db612f4db
    Signed-off-by: daiss1 <daiss1@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1339883
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Submit-Approved: Jira Key

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.c
@@ -2869,7 +2869,7 @@ static int dsi_panel_parse_topology(
 	};
 
 	if (topology_override >= 0 && topology_override < top_count) {
-		pr_info("override topology: cfg:%d lm:%d comp_enc:%d intf:%d\n",
+		pr_debug("override topology: cfg:%d lm:%d comp_enc:%d intf:%d\n",
 			topology_override,
 			topology[topology_override].num_lm,
 			topology[topology_override].num_enc,
@@ -2892,7 +2892,7 @@ static int dsi_panel_parse_topology(
 		goto parse_fail;
 	}
 
-	pr_info("default topology: lm: %d comp_enc:%d intf: %d\n",
+	pr_debug("default topology: lm: %d comp_enc:%d intf: %d\n",
 		topology[top_sel].num_lm,
 		topology[top_sel].num_enc,
 		topology[top_sel].num_intf);
@@ -2985,7 +2985,7 @@ static int dsi_panel_parse_partial_update_caps(struct dsi_display_mode *mode,
 			return 0;
 		}
 	} else {
-		pr_info("partial update disabled as the property is not set\n");
+		pr_debug("partial update disabled as the property is not set\n");
 		return 0;
 	}
 
@@ -2994,7 +2994,7 @@ static int dsi_panel_parse_partial_update_caps(struct dsi_display_mode *mode,
 
 	roi_caps->enabled = roi_caps->num_roi > 0;
 
-	pr_info("partial update num_rois=%d enabled=%d\n", roi_caps->num_roi,
+	pr_debug("partial update num_rois=%d enabled=%d\n", roi_caps->num_roi,
 			roi_caps->enabled);
 
 	if (roi_caps->enabled)
@@ -4441,12 +4441,11 @@ int dsi_panel_enable(struct dsi_panel *panel)
 			}
 			dsi_panel_trigger_panel_dead_event(panel);
 		} else {
-			pr_info("%s-. Pwr_mode(0x0A) = 0x%x\n",
-						__func__, pwr_mode);
+			pr_info("Pwr_mode(0x0A) = 0x%x\n", pwr_mode);
 			panel_recovery_retry = 0;
 		}
 	} else
-		pr_info("%s-: no_panel_on_read_support is set\n", __func__);
+		pr_info("-: no_panel_on_read_support is set\n");
 
 err:
 	mutex_unlock(&panel->panel_lock);
