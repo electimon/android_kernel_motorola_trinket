33082a25ba332fb2badefe7cc7e74de00f17a7aa
electimon <electimon@gmail.com>
2021-08-14 15:46:11 -0400

    qcacld-3.0: Checkout from moto-sm6150 lineage-18.1 branch
    Signed-off-by: electimon <electimon@gmail.com>

--- drivers/staging/qcacld-3.0/core/mac/inc/qwlan_version.h
+++ drivers/staging/qcacld-3.0/core/mac/inc/qwlan_version.h
@@ -25,16 +25,16 @@
 
    BRIEF DESCRIPTION:
    WLAN Host Version file.
-    Build number automatically updated by build scripts.
+   Build number automatically updated by build scripts.
 
    ===========================================================================*/
 
 #define QWLAN_VERSION_MAJOR            5
 #define QWLAN_VERSION_MINOR            2
-#define QWLAN_VERSION_PATCH            03
-#define QWLAN_VERSION_EXTRA            "J"
-#define QWLAN_VERSION_BUILD            26
+#define QWLAN_VERSION_PATCH            022
+#define QWLAN_VERSION_EXTRA            "U"
+#define QWLAN_VERSION_BUILD            5
 
-#define QWLAN_VERSIONSTR               "5.2.03.26J"
+#define QWLAN_VERSIONSTR               "5.2.022.5U"
 
 #endif /* QWLAN_VERSION_H */
